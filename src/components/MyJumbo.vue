5c286564f7eb92a1ce4a89dcd38d4ad964e6f14b
MikeyBadi <mihaibadia.1999@gmail.com>
2022-05-12 17:20:37 +0200

    Exercise and bonus finished

---
+++ src/components/MyJumbo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="https://www.glueglue.com/wp-content/uploads/boolean-logo.jpg" alt="Boolean Logo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyJumbo"
+}
+</script>
+
+<style>
+
+</style>
